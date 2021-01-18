f5a7b479d3a3fbc558e1ba82eb55918de7346170
zhangchaofan <zhangchaofan@tuhu.cn>
2021-01-18 14:54:55 +0800

    多线程传值

---
+++ src/main/java/com/zhang/spring/TaskDecoratorCode.java
@@ -0,0 +1,80 @@
+package com.zhang.spring;
+
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName TaskDecorator
+ * @Description: TODO
+ * @Author clark
+ * @Date 2021/1/15 14:32
+ **/
+public class TaskDecoratorCode {
+
+    public void testMDCThreadLocal() {
+        ExecutorService mainThreadPool = Executors.newFixedThreadPool(16);
+
+        ThreadPoolTaskExecutor childThreadPool = new ThreadPoolTaskExecutor();
+        childThreadPool.setCorePoolSize(2);
+        childThreadPool.setMaxPoolSize(2);
+
+        childThreadPool.setTaskDecorator(new MdcTaskDecorator());
+        childThreadPool.initialize();
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            mainThreadPool.execute(() -> {
+                //模拟在主线程设置上下文变量
+
+                Map<String, String> map = new HashMap<>();
+                map.put("test", "test" + finalI);
+                map.put("name", Thread.currentThread().getName());
+                MDC.setContextMap(map);
+                childThreadPool.execute(() -> System.out.println("子线程" + Thread.currentThread().getName() + " 获取上下文变量=" + MDC.getCopyOfContextMap()));
+                MDC.clear();
+            });
+        }
+        try {
+            childThreadPool.getThreadPoolExecutor().awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class MdcTaskDecorator implements TaskDecorator {
+
+        @Override
+        public Runnable decorate(Runnable runnable) {
+            RequestAttributes context = RequestContextHolder.currentRequestAttributes();  //1
+            System.out.println("装饰器中获取到主线程=" + Thread.currentThread().getName() + " 获取上下文 context=" + context);
+            Map<String, String> previous = MDC.getCopyOfContextMap();                      //2
+            System.out.println("装饰器中获取到主线程=" + Thread.currentThread().getName() + " 获取上下文 previous=" + previous);
+            SecurityContext securityContext = SecurityContextHolder.getContext();          //3
+            System.out.println("装饰器中获取到主线程=" + Thread.currentThread().getName() + " 获取上下文 securityContext=" + securityContext);
+
+            return () -> {
+                try {
+                    RequestContextHolder.setRequestAttributes(context);     //1
+                    MDC.setContextMap(previous);                         //2
+                    SecurityContextHolder.setContext(securityContext);   //3
+                    runnable.run();
+                } finally {
+                    RequestContextHolder.resetRequestAttributes();        // 1
+                    MDC.clear();                                        // 2
+                    SecurityContextHolder.clearContext();                // 3
+                }
+            };
+        }
+    }
+}
